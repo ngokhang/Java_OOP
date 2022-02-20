c6cb30b34a20197ea68796af635c435644fdab12
ngokhang <ngokhang2002vn@gmail.com>
2022-02-20 10:25:38 +0700

    Ex1

---
+++ BT_Tuan1/bai1.java
@@ -0,0 +1,61 @@
+/**
+ * bài 1 :
+ * Viết chương trình nhập vào một dãy số nguyên từ dang sô dòng lệnh; hiển thị
+ * và sắp xếp dãy số đó.
+ */
+package BT_Tuan1;
+
+import java.util.Scanner;
+
+public class bai1 {
+  public static int setLength() {
+    Scanner sc = new Scanner(System.in);
+    int length = sc.nextInt();
+
+    return length;
+  }
+
+  public static int[] setArray(int length) {
+    Scanner sc = new Scanner(System.in);
+    int[] arr = new int[length];
+
+    for (int i = 0; i < length; i++) {
+      arr[i] = sc.nextInt();
+    }
+
+    return arr;
+  }
+
+  public static void printArray(int[] array) {
+    System.out.println("Mang da nhap\n");
+    for (int i : array) {
+      System.out.print(i + " ");
+    }
+    System.out.println();
+  }
+
+  public static void sortArray(int[] array) {
+    System.out.println("Mang da sap xep\n");
+
+    for (int i = 0; i < array.length - 1; i++) {
+      for (int j = i + 1; j < array.length; j++) {
+        if (array[i] <= array[j]) {
+          int temp = array[i];
+          array[i] = array[j];
+          array[j] = temp;
+        }
+      }
+    }
+    System.out.println();
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int length = setLength();
+    int[] array = setArray(length);
+
+    printArray(array);
+    sortArray(array);
+    printArray(array);
+  }
+}
