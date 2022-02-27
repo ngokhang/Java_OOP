70216da9114af4a86b933634afe9ac1b5da3fed5
ngokhang <ngokhang2002vn@gmail.com>
2022-02-27 11:01:15 +0700

    BT_Tuan2

---
+++ BT_Tuan2/Bai1.java
@@ -0,0 +1,40 @@
+package BT_Tuan2;
+
+import java.util.*;
+
+class fibNumber {
+  protected long sumFibonacci(int n) {
+    if (n <= 0)
+      return 0;
+    if (n == 1)
+      return 1;
+    if (n == 2)
+      return 2;
+
+    long res = 1;
+    long temp = 1;
+    for (int i = 3; i <= n; i++) {
+      res = res + temp;
+      temp = res - temp;
+    }
+
+    return res;
+  }
+}
+
+public class Bai1 {
+  public static int setNum() {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Nhap n : ");
+    int num = sc.nextInt();
+
+    return num;
+  }
+
+  public static void main(String[] args) {
+    System.out.println("Bai 1");
+    fibNumber number = new fibNumber();
+    int n = setNum();
+    System.out.println(number.sumFibonacci(n));
+  }
+}
