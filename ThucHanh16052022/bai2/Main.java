b55e63abb5bb443855ef9621f951140ab5f94072
Phu Khang Ngo <ngokhang2002vn@gmail.com>
2022-05-16 20:56:58 +0700

    upload

--- ThucHanh16052022/bai2/Main.java
+++ ThucHanh16052022/bai2/Main.java
@@ -3,15 +3,15 @@ package ThucHanh16052022.bai2;
 public class Main {
     public static void main(String[] args) {
         double[] a = {1,2,3,4};
-        // double[] b = {2, 3, 4, 5};
+        double[] b = {2, 3, 4, 5};
         Vector v = new Vector(a);
-        // Vector x = new Vector(b);
+        Vector x = new Vector(b);
         System.out.println(v.toString());
-        // v.nhanSoThuc(5);
-        // v.calcLen();
-        // v.add(x);
-        // v.substract(x);
-        // v.nhanSoThuc(4);
+        v.nhanSoThuc(5);
+        v.calcLen();
+        v.add(x);
+        v.substract(x);
+        v.nhanSoThuc(4);
         Vector vecDonVi = v.calcUnitVector();
         System.out.println(vecDonVi.toString());
         
